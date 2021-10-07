103e58c0dce3a0c683fdd201e8dc9b6dbb1e75a6
smartkk2510 <80613483+smartkk2510@users.noreply.github.com>
2021-10-07 22:32:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -51,6 +51,9 @@ app.post("/compose", (req, res) => {
   posts.push(post);
   res.redirect("/");
 });
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log("Server started on port "+PORT);
 });
